f0b3ffced95b189842764bad1efa480fc2e0ec62
huutoi <nguyenhuutoi080196@gmail.com>
2019-11-20 00:00:50 +0700

    hello

---
+++ week1/ex_variable.py
@@ -0,0 +1,26 @@
+# variable 
+#y="john"
+#print(x)
+#print(y)
+
+# data type 
+# gom co cac kieu chuoi sau :
+#text:str
+#number: int,float,complex,
+#Sequence type : list,tuple, range
+#mapping type: dict 
+#set type: set ,frozenset
+a = frozenset(("organge","banana","apple"))
+print(a)
+##
+##
+x = 3+5j
+y = 5j
+z = -5j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+
